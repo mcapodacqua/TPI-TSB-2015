7510aa8c057e12fe69aeeaf610d3343a824fe0e2
Maxi Capodacqua <mcapodacqua@outlook.com>
2015-10-28 20:21:09 -0300

    Updates in logic, storing data

--- src/handlers/PersistentHandler.java
+++ src/handlers/PersistentHandler.java
@@ -3,12 +3,15 @@ package handlers;
 import database.ConnectionFactory;
 import java.io.File;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class PersistentHandler implements VocabularyHandler{
     private String filename;
@@ -31,15 +34,37 @@ public class PersistentHandler implements VocabularyHandler{
 
     }
 
+    private void checkKnownFile(Connection conn)
+    {
+        try (
+                PreparedStatement st = conn.prepareStatement("SELECT COUNT(*) FROM vocabulary WHERE filename LIKE ?");
+                PreparedStatement stDelete = conn.prepareStatement("DELETE FROM vocabulary WHERE filename LIKE ?");
+                ){
+            st.setString(1, filename);
+            Long countElementsSameFilename = st.executeQuery().getLong(1);
+            if (countElementsSameFilename > 0) {
+                stDelete.setString(1, filename);
+                stDelete.executeUpdate();
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(PersistentHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
     @Override
     public void processContent() {
-        try (Connection conn = ConnectionFactory.getConntection(); Statement stat = conn.createStatement();){
+        try (
+                Connection conn = ConnectionFactory.getConntection();
+                PreparedStatement stat = conn.prepareStatement("INSERT INTO vocabulary (word,count,filename) VALUES (?,?,?)");
+                ){
+            this.checkKnownFile(conn);
             for (Entry<String,Long> item : map.entrySet()) {
-                System.out.println(item.getKey() + item.getValue());
+                String word = item.getKey();
+                Long count = item.getValue();
+                stat.setString(1, word);
+                stat.setLong(2, count);
+                stat.setString(3, filename);
+                stat.executeUpdate();
             }
-            stat.close();
-            conn.close();
-
         } catch (SQLException ex) {
             System.out.println("Error " + ex.getMessage());
         }
--- src/tpi/tsb/TPITSB.java
+++ src/tpi/tsb/TPITSB.java
@@ -10,14 +10,32 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class TPITSB {
 
     public static void main(String[] args) {
+        Connection conn = ConnectionFactory.getConntection();
+//        try {
+//            conn.createStatement().execute("delete from vocabulary");
+//        } catch (SQLException ex) {
+//            Logger.getLogger(TPITSB.class.getName()).log(Level.SEVERE, null, ex);
+//        }
         File file = new File("libros" + File.separator + "test.txt");
         FileParser parser = new FileParser(file);
         PersistentHandler handler = new PersistentHandler(file);
         parser.parse(handler);
+        
+        try {
+            ResultSet rs = conn.createStatement().executeQuery("select * from vocabulary");
+            while(rs.next()) {
+                System.out.println( rs.getString(2) + "-" + rs.getLong(3) + "-" + rs.getString(4));
+            }
+            conn.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(TPITSB.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
